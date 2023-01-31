7eb80688f4f05654adfbbc0b70ef993024f03b5c
proshunsuke <shunsuke0901@gmail.com>
2023-01-31 17:19:18 +0900

    Disable application menu

--- index.js
+++ index.js
@@ -91,29 +91,7 @@ app.on('ready', () => {
     });
   });
 
-  const template = [{
-    label: 'Application',
-    submenu: [
-        {label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
-        {type: 'separator'},
-        {label: 'Quit', accelerator: 'Command+Q', click: () => {
-          app.quit();
-        }}
-    ]}, {
-      label: 'Edit',
-      submenu: [
-        {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
-        {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
-        {type: 'separator'},
-        {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
-        {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
-        {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
-        {label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'}
-      ]
-    }
-  ];
-
-  electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(template));
+  electron.Menu.setApplicationMenu(null);
 });
 
 app.on('window-all-closed', () => {
